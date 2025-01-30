74005ca6b9b06a28912c8ed04fa1df0230aeb354
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-01-29 22:07:06 -0500

    add data types notes and assignment

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -6,8 +6,23 @@ Declare variables called country, continent and population and assign their valu
 Log their values to the console.
 
 let country = "United States";
+let population = 18000000;
 let continent = "North America";
 console.log(country);
 console.log(continent)
 */
+
+// Data Types
+/*
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+let isIsland = false;
+let language;
+
+// Log the types of isIsland, population, country and language to the console.
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+*/
+
 
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -7,6 +7,7 @@ if (js === "amazing") {
 */
 
 // Values and Variables Lecture
+/*
 console.log('Jonas') // The value is Jonas
 console.log(23) // The value is 23
 // We can store values into variables
@@ -14,5 +15,41 @@ let namey = 'Jonas';
 var number = 23;
 console.log(namey) // We can now change the value in one place, it is stored in our memory.
 // camelCase -> proper variable naming convention
+*/
 
 // DATA TYPES
+/*
+Primitve data types are any data types that are not objects -> There are 7 primitive data types
+
+Number: Floating point numbers -> used for decimals and integers
+
+String: Sequence of characters -> used for text
+
+Boolean: Logical type can only be true or false -> used for taking decisions
+
+Undefined: Value taken by a variable that is not yet defined
+
+Null: Also means 'empty value'
+
+Symbol (ES2015): Value that is unique and cannot be changed
+
+BigInt (ES2020): Larger integers than the Number type can hold
+
+JavaScript has dynamic typing, we dont not have to manually define the data type of the value stored in a variable. The value has a type not a variable.
+
+
+// boolean
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+// we can use an operator to check type
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+let year; // will be undefined
+console.log(year); // value and variable types will both be undefined
+let weirdBug = null;
+console.log(typeof weirdBug); // returns object (bug) should be null type
+*/
+
+// let, const and var
