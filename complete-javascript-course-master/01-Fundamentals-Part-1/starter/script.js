dc5b541101bc770a50f078fd0bb174f52559daee
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-01-29 20:13:42 -0500

    add values and variables content

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,17 @@
+// Introduction and linking JS file lectures
+/*
 let js = "amazing";
 if (js === "amazing") {
   alert("JavaScript is fun!");
 }
+*/
+
+// Values and Variables Lecture
+console.log('Jonas') // The value is Jonas
+console.log(23) // The value is 23
+// We can store values into variables
+let namey = 'Jonas';
+var number = 23;
+console.log(namey) // We can now change the value in one place, it is stored in our memory.
+// camelCase -> proper variable naming convention
+
