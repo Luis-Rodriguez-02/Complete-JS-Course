8873027a9ffe5d0c95c31d081f5cef052156b7ac
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-10 20:13:29 -0500

    add logical operator assignment

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -113,7 +113,8 @@ if (numNeighbours === 1) {
 
 // Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks English, has less than 50 million people and is not an island.
 // Write an if statement to help Sarah figure out if your country is right for her. You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution if necessary.
-if (language === 'English' && population < 50 && !isIsland) {
+if (language === 'English' && population < 50
+   && !isIsland) {
   console.log(`You should live in ${country} :)`); 
 } else {
   console.log(`${country} does not meet your criteria :(`)
