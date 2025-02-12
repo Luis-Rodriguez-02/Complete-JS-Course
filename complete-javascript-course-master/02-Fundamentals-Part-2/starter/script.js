760c4a8068bb9820d34f65095a3487c7db6ea3a0
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-11 19:57:37 -0500

    add strict mode lesson and files

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,20 @@
+// Activating Strict Mode
+'use strict';
+
+/*
+
+// strict mode -> strict mode makes us write safer code
+// creates visible errors and prevents us from doing certain things
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicense = true;
+}
+
+if (hasDriversLicense) {
+  console.log("I can drive");
+}
+
+*/
