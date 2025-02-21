2cd85940f8396d79b70b035fd605e30722b451d1
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-20 19:00:58 -0500

    add guess my number project start

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 19;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+document.querySelector('.guess').value;
+*/
