a55197f9e2f6fc66e20f273a6d28c2cf4fa8467f
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-20 19:32:42 -0500

    add click functionality

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -8,3 +8,36 @@ document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 23;
 document.querySelector('.guess').value;
 */
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⚠️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number! 🎊';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high! 📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost! 😭';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low! 📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost! 😭';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
