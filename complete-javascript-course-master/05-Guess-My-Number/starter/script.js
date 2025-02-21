4c467e893fc8c266dee773602e838d5f3914a45d
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-21 13:23:18 -0500

    add guess my number final project

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -9,9 +9,9 @@ document.querySelector('.guess').value = 23;
 document.querySelector('.guess').value;
 */
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -21,18 +21,18 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⚠️ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🎊';
-  } else if (guess > secretNumber) {
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) highScore = score;
+
+    document.querySelector('.highscore').textContent = highScore;
+    // when player wins -> change background
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high! 📈';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost! 😭';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low! 📉';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high! 📈' : 'Too low! 📉';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -41,3 +41,19 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+// game reset, reset lives
+const againBtn = document
+  .querySelector('.again')
+  .addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing.....';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+
+    document.querySelector('.guess').value = '';
+  });
