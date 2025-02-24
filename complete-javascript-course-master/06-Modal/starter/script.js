5cf1b2e4fe25b109de45ff511494ff5ffe032ea3
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-23 20:10:29 -0500

    finish modal project

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -20,5 +20,13 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 btnCloseModal.addEventListener('click', modalHidden);
-
 overlay.addEventListener('click', modalHidden);
+
+document.addEventListener('keydown', function (e) {
+  // every time event occurs an object is created and we can access that event object inside the handler function
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      modalHidden();
+    }
+  }
+});
