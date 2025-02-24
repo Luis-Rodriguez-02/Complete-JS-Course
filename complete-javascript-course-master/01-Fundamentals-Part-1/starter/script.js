a76fe99c37a1fe1e4174966219fe854daf907a3f
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-02-24 18:46:09 -0500

    add files

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -351,7 +351,7 @@ if (age >= 18) {
 
 console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`)
 
-*/
+
 // CODING CHALLENGE #4
 
 // Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
@@ -367,3 +367,5 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was $
 // Example: The bill was 275, the tip was 41.25, and the total value 316.25.
 
 // Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+*/
--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+function resetGame() {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  dice.classList.add('hidden');
+}
+
+resetGame();
